ea85578527f037e891a9553c633b7ae97a537d37
sunil <sunil.giri@presageinsights.ai>
2025-07-16 17:49:12 +0530

    added api v1

--- embryva_backend/settings.py
+++ embryva_backend/settings.py
@@ -128,7 +128,7 @@ CELERY_ENABLE_UTC = True
 
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', 'your_stripe_secret_key')
 STRIPE_WEBHOOK_SECRET = os.getenv('STRIPE_WEBHOOK_SECRET', 'your_stripe_webhook_secret')
-STRIPE_REDIRECT_DOMAIN = os.getenv('FRONTEND_URL', 'http://ixwiser.com') 
+STRIPE_REDIRECT_DOMAIN = os.getenv('FRONTEND_URL', 'http://ixwiser.com/api/v1') 
 
 CELERY_BEAT_SCHEDULE = {
     'send-meeting-reminder-emails': {
