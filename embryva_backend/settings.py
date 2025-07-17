c3007565da4094d0e62091b78342db35a8229650
sunil <sunil.giri@presageinsights.ai>
2025-07-17 19:13:54 +0530

    increased jwt time

--- embryva_backend/settings.py
+++ embryva_backend/settings.py
@@ -182,7 +182,7 @@ AUTHENTICATION_BACKENDS = [
 # JWT Configuration
 from datetime import timedelta
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'ACCESS_TOKEN_LIFETIME': timedelta(hours=12),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
